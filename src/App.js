88a970a56948ce10cd533ca05088d423661dd686
tanates <timur876376.ru@mail.ru>
2022-08-12 20:51:20 +0700

    lol

--- src/App.js
+++ src/App.js
@@ -7,33 +7,3 @@ document.getElementById("btn_enter").onclick = function check_login() {
     alert("Пользователь не найден");
   }
 };
-
-function img_timer() {
-  var total_img = 2;
-  var interval = 10000;
-  var time_out = 1;
-  var i = 0;
-  var timeout;
-  var opacity = 100;
-  opacity--;
-  var k = i + 1;
-  var image_now = "flower_" + i;
-  if (i == total_img) {
-    k = 1;
-  }
-  document.getElementById(image_now).style.opacity = opacity / 100;
-  document.getElementById(image_now).style.filter =
-    "alpha(opacity=" + opacity + ")";
-  document.getElementById(image_next).style.opacity = (100 - opacity) / 100;
-  document.getElementById(image_next).style.filter =
-    "alpha(opacity=" + (100 - opacity) + ")";
-  if (opacity == 1) {
-    opacity = 100;
-    clearTimeout(timeout);
-  }
-  setInterval(function () {
-    i++;
-    if (i > total_pics_num) i = 1;
-    fade_to_next();
-  }, interval);
-}
